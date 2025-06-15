438372cbe5dc3e9323907539ce91bbfa5d1ab6e5
Adith <adithpv2014@gmail.com>
2025-06-15 18:10:21 +0530

    added axios instance

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    } else if (error.response?.status === 500) {
+      console.error("Server error, Please try again later");
+    } else if (error?.code === "ENCONNABORTED") {
+      console.error("Request timeout, Please try again later");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
