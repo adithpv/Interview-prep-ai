d4ad7cfe7d4eb5b13d9e7ecec1419cef378b93ff
Adith Pv <adithpv2014@gmail.com>
2025-09-06 21:04:30 +0530

    changes in link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="backdrop-blur=[2px] h-16 border-b border-gray-200/50 bg-white py-2.5">
       <div className="container mx-auto flex items-center justify-between gap-5">
-        <Link to="/dashboard">
+        <Link to="/">
           <h2 className="text-lg leading-5 font-medium text-black md:text-xl">
             Interview Ready AI
           </h2>
